9937fa130587df04e782317fab14bcd4100cc6b9
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-28 01:46:41 +0100

    finished bottomNavigationBar

---
+++ lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,45 @@
+import 'package:fazebook/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatelessWidget {
+  final List<IconData> icons;
+  final int selectedIndex;
+  final Function(int) onTap;
+  const CustomTabBar(
+      {Key? key,
+      required this.icons,
+      required this.selectedIndex,
+      required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      onTap: onTap,
+      indicatorPadding: EdgeInsets.zero,
+      indicator: const BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Palette.facebookBlue, width: 4),
+        ),
+      ),
+      tabs: icons
+          .asMap()
+          .map(
+            (i, e) => MapEntry(
+              i,
+              Tab(
+                icon: Icon(
+                  e,
+                  color: i == selectedIndex
+                      ? Palette.facebookBlue
+                      : Colors.black45,
+                      size: 30,
+                ),
+              ),
+            ),
+          )
+          .values
+          .toList(),
+    );
+  }
+}
